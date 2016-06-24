a56a49b047699953c730dc2f5a3835059d398912
linwei-yoh <395373035@qq.com>
2016-06-24 17:21:24 +0800

    扩展输入数据长度 增加数据格式

--- app/src/main/java/com/example/android/hexcon/MainActivity.java
+++ app/src/main/java/com/example/android/hexcon/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,20 +29,28 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            String DecVal,HexVal;
+            String DecVal,HexVal,BinVal;
             ET_dec.removeTextChangedListener(DecTextWatcher);
             ET_hex.removeTextChangedListener(HexTextWatcher);
+
+            BinVal = charSequence.toString().replace(" ","");
             try {
-                DecVal = Integer.valueOf(charSequence.toString(),2).toString();
-                HexVal = Integer.toHexString(Integer.valueOf(DecVal));
+                DecVal = Long.valueOf(BinVal,2).toString();
+                HexVal = Long.toHexString(Long.valueOf(DecVal));
             }
             catch (NumberFormatException e)
             {
                 DecVal = "";
                 HexVal = "";
             }
-            ET_dec.setText(DecVal);
-            ET_hex.setText(HexVal);
+            ET_dec.setText(DecFormat(DecVal));
+            ET_hex.setText(HexFormat(HexVal));
+
+            ET_bin.removeTextChangedListener(this);
+            String DisVal = BinFormat(BinVal);
+            ET_bin.setText(DisVal);
+            ET_bin.setSelection(DisVal.length());
+            ET_bin.addTextChangedListener(this);
         }
 
         @Override
@@ -63,31 +70,32 @@ public class MainActivity extends AppCompatActivity {
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             String BinVal,HexVal,DecVal;
 
-            ET_dec.removeTextChangedListener(DecTextWatcher);
             ET_bin.removeTextChangedListener(BinTextWatcher);
             ET_hex.removeTextChangedListener(HexTextWatcher);
-            Log.e("DEBUG","afterChanged");
 
+            DecVal = charSequence.toString().replace(",","");
             try {
-                BinVal = Integer.toBinaryString((Integer.valueOf(charSequence.toString())));
-                HexVal = Integer.toHexString(Integer.valueOf(charSequence.toString())).toUpperCase();
+                BinVal = Long.toBinaryString((Long.valueOf(DecVal)));
+                HexVal = Long.toHexString(Long.valueOf(DecVal)).toUpperCase();
             }
             catch (NumberFormatException e)
             {
                 BinVal = "";
                 HexVal = "";
             }
+            ET_bin.setText(BinFormat(BinVal));
+            ET_hex.setText(HexFormat(HexVal));
 
-            Log.e("DEBUG","转化完成" + "BinVal:" + BinVal +"\n" + "HexVal:" + HexVal);
-            ET_bin.setText(BinVal);
-            ET_hex.setText(HexVal);
-            Log.e("DEBUG","赋值");
+            ET_dec.removeTextChangedListener(this);
+            String DisVal = DecFormat(DecVal);
+            ET_dec.setText(DisVal);
+            ET_dec.setSelection(DisVal.length());
+            ET_dec.addTextChangedListener(this);
         }
 
         @Override
         public void afterTextChanged(Editable editable) {
             ET_bin.addTextChangedListener(BinTextWatcher);
-            ET_dec.addTextChangedListener(DecTextWatcher);
             ET_hex.addTextChangedListener(HexTextWatcher);
         }
     };
@@ -100,12 +108,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            String DecVal,BinVal;
+            String DecVal,BinVal,HexVal;
             ET_bin.removeTextChangedListener(BinTextWatcher);
             ET_dec.removeTextChangedListener(DecTextWatcher);
+
+            HexVal = charSequence.toString().replace(" ","");
             try {
-                DecVal = Integer.valueOf(charSequence.toString(),16).toString();
-                BinVal = Integer.toBinaryString(Integer.valueOf(charSequence.toString(),16));
+                DecVal = Long.valueOf(HexVal,16).toString();
+                BinVal = Long.toBinaryString(Long.valueOf(DecVal));
             }
             catch (NumberFormatException e)
             {
@@ -113,8 +123,14 @@ public class MainActivity extends AppCompatActivity {
                 BinVal = "";
             }
 
-            ET_bin.setText(BinVal);
-            ET_dec.setText(DecVal);
+            ET_bin.setText(BinFormat(BinVal));
+            ET_dec.setText(DecFormat(DecVal));
+
+            ET_hex.removeTextChangedListener(this);
+            String DisVal = HexFormat(HexVal.toUpperCase());
+            ET_hex.setText(DisVal);
+            ET_hex.setSelection(DisVal.length());
+            ET_hex.addTextChangedListener(this);
         }
 
         @Override
@@ -124,8 +140,26 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private String BinFormat(String string){
+        StringBuffer buffer = new StringBuffer(string);
+        StringBuffer Result = new StringBuffer();
+        Result.append(buffer.reverse().toString().replaceAll("\\d{4}(?!$)","$0 "));
+        return Result.reverse().toString();
+    }
 
+    private String DecFormat(String string){
+        StringBuffer buffer = new StringBuffer(string);
+        StringBuffer Result = new StringBuffer();
+        Result.append(buffer.reverse().toString().replaceAll("\\d{3}(?!$)","$0,"));
+        return Result.reverse().toString();
+    }
 
+    private String HexFormat(String string){
+        StringBuffer buffer = new StringBuffer(string);
+        StringBuffer Result = new StringBuffer();
+        Result.append(buffer.reverse().toString().replaceAll("\\w{4}(?!$)","$0 "));
+        return Result.reverse().toString().toUpperCase();
+    }
 
 }
 
